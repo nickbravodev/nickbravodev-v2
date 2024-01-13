7806107af32f031ad1fda85d38bec7d665956be1
Nick Bravo <nick@nickbravo.dev>
2024-01-13 16:02:45 -0700

    components and css structure

--- src/data/socialData.js
+++ src/data/socialData.js
@@ -1,27 +1,27 @@
 const socialData = [
   {
-    name: 'Github',
-    path: 'https://www.github.com/nickbravodev/',
-    target: '_blank',
-    icon: 'github'
+    name: "Github",
+    path: "https://www.github.com/nickbravodev/",
+    target: "_blank",
+    icon: "social/github",
   },
   {
-    name: 'Twitter',
-    path: 'https://www.twitter.com/nickbravodev/',
-    target: '_blank',
-    icon: 'twitter'
+    name: "Twitter",
+    path: "https://www.twitter.com/nickbravodev/",
+    target: "_blank",
+    icon: "social/twitter",
   },
   {
-    name: 'Instagram',
-    path: 'https://www.instagram.com/nickbravodev/',
-    target: '_blank',
-    icon: 'instagram'
+    name: "Instagram",
+    path: "https://www.instagram.com/nickbravodev/",
+    target: "_blank",
+    icon: "social/instagram",
   },
   {
-    name: 'LinkedIn',
-    path: 'https://www.linkedin.com/in/nickbravodev/',
-    target: '_blank',
-    icon: 'linkedin'
+    name: "LinkedIn",
+    path: "https://www.linkedin.com/in/nickbravodev/",
+    target: "_blank",
+    icon: "social/linkedin",
   },
 ];
 
