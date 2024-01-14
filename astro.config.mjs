2b8a8b54ce17637bd0583f6eca0246236906aa1b
Nick Bravo <nick@nickbravo.dev>
2024-01-13 19:28:16 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
     htmlBeautifier({
       indent_size: 2,
       end_with_newline: true,
-      preserve_newlines: false,
+      preserve_newlines: true,
       max_preserve_newlines: 2,
       space_in_paren: true,
       space_in_empty_paren: false,
