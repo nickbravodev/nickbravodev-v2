e04b5e7941f68aefde8e37332385358cc97d3656
Nick Bravo <nick@nickbravo.dev>
2024-03-03 14:38:46 -0700

    updates

--- .netlify/functions-internal/ssr/chunks/4-arcane-ops_CTlzfgR4.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./4-arcane-ops_4wnp2jUx.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- .netlify/functions-internal/ssr/chunks/index_mglG7iTD.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_K9oGhyMs.mjs').then(n => n.a);
-
-export { page };
