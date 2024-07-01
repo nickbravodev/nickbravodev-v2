69add1d89f71f00ed5ae9cc8c41d73fa6f2be9ac
Nick Bravo <nick@nickbravo.dev>
2024-07-01 09:06:12 -0600

    resume updates

---
+++ src/data/resumeExperience.js
@@ -0,0 +1,150 @@
+const resumeExperience = [
+  {
+    position: "Principal Web Developer",
+    employer: "Maverik",
+    city: "Salt Lake City",
+    state: "UT",
+    startDate: "July 2023",
+    endDate: "Present",
+    synopsis:
+      "Recruited to this role to oversee codebases of all Maverik, Kum & Go, and FJ Management (FJM) web properties, including primary websites for all companies, and the combined Careers website.",
+    duties: [
+      {
+        task: "Completely rebuilt the Kum & Go public website from scratch (including CMS and API integration) within a tight deadline of four weeks, which significantly reduced page load times, increased accessibility and performance, and saved the company over $600,000 in annual costs.",
+      },
+      {
+        task: "Architected scalable codebases for Maverik and Kum & Go web properties using HTML, CSS, JavaScript, JSX, and TypeScript within the Astro framework, effectively utilizing complex web components, content collections, and routing.",
+      },
+      {
+        task: "Constructed converting landing pages for active Maverik and Kum & Go marketing campaigns, sweepstakes, and loyalty programs.",
+      },
+      {
+        task: "Enhanced workflow efficiency between the Digital Product Team and the rest of the Marketing and Public Relations Teams to streamline web updates across all websites.",
+      },
+    ],
+  },
+  {
+    position: "Web Development Consultant",
+    employer: "Astropost",
+    city: "Salt Lake City",
+    state: "UT",
+    startDate: "March 2022",
+    endDate: "Present",
+    synopsis:
+      "Transitioned freelance business to a systemized business with streamlined client onboarding and project management workflow.",
+    duties: [
+      {
+        task: "Implemented complex web architecture strategies for fast websites that meet high-performance metrics.",
+      },
+      {
+        task: "Built scalable codebases for privacy-focused websites designed for headless CMS and API integration.",
+      },
+      {
+        task: "Designed and built an accessible digital layout for Unredacted Magazine.",
+      },
+    ],
+  },
+  {
+    position: "Adjunct Faculty - Web Design & Development",
+    employer: "Salt Lake Community College",
+    city: "Salt Lake City",
+    state: "UT",
+    startDate: "December 2023",
+    endDate: "Present",
+    synopsis: "",
+    duties: [
+      {
+        task: "Instructed students in novice to advanced UI/UX and web design and development courses.",
+      },
+      {
+        task: "Taught students the fundamentals of web design and development, including Semantic HTML, CSS, JavaScript, responsive layouts, and accessibility practices.",
+      },
+      {
+        task: "Demonstrated real-world project management and DevOps workflows and best practices.",
+      },
+    ],
+  },
+  {
+    position: "Freelance Designer / Developer",
+    employer: "Freelance",
+    city: "Salt Lake City",
+    state: "UT",
+    startDate: "May 2007",
+    endDate: " March 2022",
+    synopsis:
+      "Provided freelance web design and development services to local businesses, including several pro bono projects.",
+    duties: [
+      {
+        task: "Built engaging websites, user interfaces, visual identities, digital products, and online assets.",
+      },
+      {
+        task: "Constructed many pro bono websites for public safety, political, and nonprofit organizations.",
+      },
+    ],
+  },
+  {
+    position: "Graphic Designer / Web Developer",
+    employer: "8fish",
+    city: "Sandy",
+    state: "UT",
+    startDate: "June 2008",
+    endDate: "September 2009",
+    synopsis:
+      "Recruited as a graphic designer and illustrator, quickly transitioned to primary web developer due to previous experience.",
+    duties: [
+      {
+        task: "Developed the website for Maverik’s Kick Start, a local adventure TV show produced by Maverik, a leading convenience store chain.",
+      },
+      {
+        task: "Elevated brands by building powerful websites, and designing graphics, social media content, and other digital assets.",
+      },
+    ],
+  },
+  {
+    position: "Police Officer",
+    employer: "University Of Utah Police Department",
+    city: "Salt Lake City",
+    state: "UT",
+    startDate: "March 2021",
+    endDate: "July 2023",
+    synopsis: "",
+    duties: [
+      {
+        task: "Spearheaded educational and community outreach campaigns as a liaison between the police and marketing departments.",
+      },
+    ],
+  },
+  {
+    position: "Police Detective",
+    employer: "Price City Police Department",
+    city: "Price",
+    state: "UT",
+    startDate: "November 2015",
+    endDate: "March 2021",
+    synopsis: "",
+    duties: [
+      {
+        task: "Developed and oversaw a comprehensive training and equipment program.",
+      },
+    ],
+  },
+  {
+    position: "Police Lieutenant",
+    employer: "Wellington Police Department",
+    city: "Wellington",
+    state: "UT",
+    startDate: "August 2010",
+    endDate: "November 2015",
+    synopsis: "",
+    duties: [
+      {
+        task: "Improved transparency and community relationships by building the department's first website and social media channels and generating timely press releases.",
+      },
+      {
+        task: "Developed and implemented a Reserve Officer Program that reduced personnel expenditures by 20% and increased shift coverage by 50%.",
+      },
+    ],
+  },
+];
+
+export default resumeExperience;
