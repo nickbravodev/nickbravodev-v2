7ff00f8eecef949eb50fbd76121854b4a7c5a60e
NickBravo.dev <102766832+nickbravodev@users.noreply.github.com>
2025-03-07 11:52:58 -0700

    packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import vercel from "@astrojs/vercel";
 
 import opengraphImages, { presets } from "astro-opengraph-images";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   // output: "server",
@@ -23,34 +25,27 @@ export default defineConfig({
   //     destination: "https://nickbravo.dev/work/$1",
   //   },
   // },
-  integrations: [
-    icon(),
-    sitemap(),
-    robotsTxt(),
-    mdx(),
-    htmlBeautifier({
-      indent_size: 2,
-      end_with_newline: true,
-      preserve_newlines: true,
-      max_preserve_newlines: 2,
-      space_in_paren: true,
-      space_in_empty_paren: false,
-      wrap_line_length: 0,
-    }),
-    opengraphImages({
-      options: {
-        fonts: [
-          {
-            name: "Roboto",
-            weight: 400,
-            style: "normal",
-            data: fs.readFileSync("node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff"),
-          },
-        ],
-      },
-      render: presets.backgroundImage,
-    }),
-  ],
+  integrations: [icon(), sitemap(), robotsTxt(), mdx(), htmlBeautifier({
+    indent_size: 2,
+    end_with_newline: true,
+    preserve_newlines: true,
+    max_preserve_newlines: 2,
+    space_in_paren: true,
+    space_in_empty_paren: false,
+    wrap_line_length: 0,
+  }), opengraphImages({
+    options: {
+      fonts: [
+        {
+          name: "Roboto",
+          weight: 400,
+          style: "normal",
+          data: fs.readFileSync("node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff"),
+        },
+      ],
+    },
+    render: presets.backgroundImage,
+  }), react()],
 
   adapter: vercel(),
 });
